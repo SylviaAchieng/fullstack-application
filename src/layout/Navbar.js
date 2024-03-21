f67509697ec9719edfefc25f772d5f7adff75b13
SylviaAchieng <achiengsylvia157@gmail.com>
2024-03-21 15:37:10 +0300

    update

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -7,7 +7,7 @@ export default function Navbar() {
       
       <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">Crud Application</a>
+          <Link className="navbar-brand" to='/'>Crud Application</Link>
           <button 
           className="navbar-toggler" 
           type="button" 
--- src/users/ViewUsers.js
+++ src/users/ViewUsers.js
@@ -1,7 +1,29 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { Link, useParams } from 'react-router-dom'
+
+
 
 export default function ViewUsers() {
+
+  const [user, setUser]=useState({
+    name:"",
+    username:"",
+    email:""
+  })
+
+  const {id}=useParams();
+
+  useEffect(()=>{
+    loadUser()
+
+  }, [])
+
+  const loadUser=async ()=>{
+    const result=await axios.get(`http://localhost:8080/user/${id}`)
+    setUser(result.data);
+  }
+
   return (
     <div className='container'>
       <div className='row'>
@@ -9,16 +31,19 @@ export default function ViewUsers() {
             <h2 className='text-center m-4'>User Details</h2>
             <div className='card'>
                 <div className='card-header'>
-                    Details of user id:
+                    Details of user id: {user.id}
                     <ul className='list-group list-group-flush'>
                         <li className='list-group-item'>
                             <b>Name:</b>
+                            {user.name}
                         </li>
                         <li className='list-group-item'>
                             <b>UserName:</b>
+                            {user.username}
                         </li>
                         <li className='list-group-item'>
                             <b>Email:</b>
+                            {user.email}
                         </li>
                     </ul>
                 </div>
